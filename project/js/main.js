82f117b3c174d4dc77f305688ed89b6da5b1f228
Louis Chausse <lchausse@auxilio.io>
2019-09-16 17:19:21 -0400

    create an API request to get weather data for Boston

--- project/js/main.js
+++ project/js/main.js
@@ -1,9 +1,12 @@
-function showPicture(){
-  // use jQuery ($ is shorthand) to find the div on the page and then change the html
-  // 'rounded-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#image").append('<img class="rounded-circle" src="images/high-five.gif"/>');
-  $("p").html("High five! You're building your first web app!");
+$(document).ready(function(){
+  getWeather();
+})
 
-  // jQuery can do a lot of crazy stuff, so make sure to Google around to find out more
-  
+function getWeather() {
+  var url = "https://api.openweathermap.org/data/2.5/weather?q=Boston&units=imperial&APPID="+apiKey;
+
+  $.ajax(url,{success: function(data){
+    $(".city").text(data.name);
+    $(".temp").text(data.main.temp);
+  }})
 }
